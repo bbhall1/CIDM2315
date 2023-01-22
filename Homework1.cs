5c79571bed55ce8edc3292aefa80ca5d538352aa
bbhall1 <122958532+bbhall1@users.noreply.github.com>
2023-01-21 21:36:56 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Z = 4X2+3Y, when  X = 2.5, Y=3.3
+        double x = 2.5;
+        double y = 3.3;
+        double z;
+        // setting up X value
+        Console.Write("X= " + x + ",");
+        // setting up Y value, same line as X value with a tab space
+        Console.WriteLine("\tY= " + y);
+        // Z value plus formula to calculate Z
+        Console.WriteLine("The value of Z is: " + (z=4*x*x+3*y));
+    }
+}
